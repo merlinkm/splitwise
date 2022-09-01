708b54e2ef9191f678dc8f9b8b56bb4c5bbddb85
merlinkm <merlinkmm@gmail.com>
2022-09-01 10:53:16 +0530

    modify style tags

--- resources/views/user_group/expenseDetails.blade.php
+++ resources/views/user_group/expenseDetails.blade.php
@@ -1,16 +1,5 @@
 @extends('layout.app')
-@section('style')
-    <link rel="stylesheet" type="text/css" href="../files/assets/icon/themify-icons/themify-icons.css">
 
-    <link rel="stylesheet" type="text/css" href="../files/assets/icon/icofont/css/icofont.css">
-    <link rel="stylesheet" type="text/css"
-        href="../files/bower_components/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
-    <!-- <link rel="stylesheet" type="text/css" href="../files/assets/pages/data-table/css/buttons.dataTables.min.css"> -->
-    <link rel="stylesheet" type="text/css"
-        href="../files/bower_components/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css">
-
-    <link rel="stylesheet" type="text/css" href="../files/assets/css/pages.css">
-@endsection
 @section('content')
     <div class="page-header card">
         <div class="row align-items-end">
@@ -83,13 +72,4 @@
             </div>
         </div>
     </div>
-@endsection
-
-@section('script')
-    <script src="../files/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="../files/bower_components/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <script src="../files/bower_components/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="../files/bower_components/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
-
-    <script src="../files/assets/pages/data-table/js/data-table-custom.js"></script>
 @endsection
